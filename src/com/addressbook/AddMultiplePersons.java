3e68d9ee109419475d8b72e85ba2975430b5010c
Sangepu Pranay Kumar <pranaychinna12@gmail.com>
2023-01-14 01:43:18 +0530

    Creating a AddressBook and Adding multiple persons into Address Book

---
+++ src/com/addressbook/AddMultiplePersons.java
@@ -0,0 +1,44 @@
+package com.addressbook;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class AddMultiplePersons {
+    AddressBookMain details = new AddressBookMain();
+    ArrayList<AddressBookMain> listOfContacts = new ArrayList<>();
+    public void addContactDetails(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the First name :");
+        details.setFirstName(scanner.next());
+        System.out.println("Enter the Last name :");
+        details.setLastName(scanner.next());
+        System.out.println("Enter the Address :");
+        details.setAddress(scanner.next());
+        System.out.println("Enter the City :");
+        details.setCity(scanner.next());
+        System.out.println("Enter the State :");
+        details.setState(scanner.next());
+        System.out.println("Enter the Zip :");
+        details.setZip(scanner.next());
+        System.out.println("Enter the Mobile Number :");
+        details.setPhoneNumber(scanner.next());
+        System.out.println("Enter the Email :");
+        details.setEmail(scanner.next());
+        AddressBookMain contacts = new AddressBookMain(details.getFirstName(), details.getLastName(),details.getAddress(),details.getCity(),details.getState(),details.getZip(),details.getPhoneNumber(),details.getEmail());
+        listOfContacts.add(contacts);
+    }
+    public void printContactDetails(){
+        System.out.println(listOfContacts);
+    }
+
+    public static void main(String[] args) {
+        AddMultiplePersons obj = new AddMultiplePersons();
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter the number of Contacts");
+        int number = scanner.nextInt();
+        for (int i=1;i<=number;i++){
+            obj.addContactDetails();
+        }
+        obj.printContactDetails();
+    }
+}
